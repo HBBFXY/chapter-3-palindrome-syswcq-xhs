ba65d8567c9a6990bbd26d4f61a61b9707f33882
syswcq-xhs <481124168@qq.com>
2025-09-19 10:54:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-s = input("请输入一个5位数字：")
-if len(s) != 5 or not s.isdigit():
+n = input("请输入一个5位数字：")
+if len(n) != 5 or not n.isdigit():
     print("错误提示：请输入一个5位纯数字")
 else:
-    if s == s[::-1]:
+    num_list = list(n)
+    left, right = 0, 4
+    is_palindrome = True
+    while left < right:
+        if num_list[left] != num_list[right]:
+            is_palindrome = False
+            break
+        left += 1
+        right -= 1
+    if is_palindrome:
         print("是回文数")
     else:
         print("不是回文数")
